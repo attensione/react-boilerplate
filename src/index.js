ee4c66b6a64dba65b63ac4922026791cf8079e14
Attenzione <atrobz@gmail.com>
2023-02-07 15:17:16 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App_users';
+//import App from './App_users';
 //import App from './App_currencies';
-//import reportWebVitals from './reportWebVitals';
+import App from './App_random_users';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
@@ -11,4 +13,5 @@ root.render(<React.StrictMode><App /></React.StrictMode>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//import reportWebVitals from './reportWebVitals';
 //reportWebVitals(console.log);
